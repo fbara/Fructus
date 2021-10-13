78c9ec45e2d77cfce5a09dc5beb146d65c57cfe0
fbara <fbara1@yahoo.com>
2021-10-13 10:25:23 -0500

    Add onboarding tabs and models.

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -11,7 +11,8 @@ struct OnboardingView: View {
     var body: some View {
         TabView {
             ForEach(0..<5) { item in
-                FruitCardView()
+                //FruitCardView()
+                Text("Cards")
             }
             
         }
